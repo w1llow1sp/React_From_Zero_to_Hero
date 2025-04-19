3306d95975a860355acb215ce06e770265c1b611
Mary <mari.front.74@gmail.com>
2025-04-19 15:29:01 +0500

    add new material

---
+++ Chapter 1/1.Typeof.js
@@ -0,0 +1,27 @@
+
+let str = "Hello";
+
+let nr = 7;
+
+let bigNr = 12345678901234n;
+
+let bool = true;
+let str1 = 'Laurence';
+
+let str2 = "Svekis";
+
+let val1 = undefined;
+
+let val2 = null;
+
+let myNum = 1000;
+
+console.log(str, typeof str) //string
+console.log(nr, typeof nr) //number
+console.log(bigNr, typeof bigNr) //bigint
+console.log(bool, typeof bool) //boolean
+console.log(str1, typeof str1) //string
+console.log(str2, typeof str2) //string
+console.log(val1, typeof val1) //undefined
+console.log(val2, typeof val2) // object
+console.log(myNum, typeof myNum) // number
---
+++ Chapter 1/Practice 2-2.js
@@ -0,0 +1,15 @@
+let myName ='Maaike'
+let myAge = '29'
+let canIProgaming = true
+
+/**
+ * Создайте переменную для своего имени, еще одну — для вашего возраста, и еще одну — для строки, определяющей, умеете ли вы кодировать в JavaScript или нет.
+ *
+ * Выведите на экран следующее суждение, где name, age и true/false будут переменными:
+ *
+ * Hello, my name is Maaike, I am 29 years old and I can code
+ *
+ * JavaScript: true.
+ */
+
+console.log(`Hello, my name is ${myName}, I am ${myAge} years old and ${canIProgaming ? 'I can code' : "I can't code"}`)
